34c81e644225874b5cde72cd6c6e4ebbe5c586a7
Mulaimboy <bscs23059@itu.edu.pk>
2026-06-25 16:48:55 +0500

    New: Status enum for new session column

--- app/schemas/sessions.py
+++ app/schemas/sessions.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from app.utils.genrateQuestions import ExperienceLevel, QuestionType, Difficulty
+from enum import Enum
 
 class CreateSession(BaseModel):
     userid: str
@@ -7,6 +8,16 @@ class CreateSession(BaseModel):
     role: str
     level: ExperienceLevel
 
+class Status(str,Enum):
+    complete ="complete"
+    inprogress="inprogress"
+
+
 class GenerateQuestionRequest(BaseModel):
     question_type: QuestionType
     difficulty: Difficulty
+
+
+class SessionComplete(BaseModel):
+    userid: str
+    sessionid:str
